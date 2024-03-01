1c0792e8ece281d39476f87c83d7aaa6109300bf
marco <pokecraftmod@gmail.com>
2024-03-01 16:49:17 -0500

    modificando

--- maquina.js
+++ maquina.js
@@ -7,6 +7,7 @@ const logger = require('./loggerMiddleware')
 // const http = require('http');
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 app.use(logger)
 
@@ -108,7 +109,7 @@ app.use((req,res)=>{
     res.status(404).json({message: 'Not found'})
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)

4753d27db56babfa57a4c73d88df1b8d54edfd7a
marco <pokecraftmod@gmail.com>
2024-03-01 16:51:48 -0500

    modificandox2

--- maquina.js
+++ maquina.js
@@ -32,9 +32,9 @@ let notes = [
     }
 ]
 
-app.get('/',(req,res) => {
-    res.send('<h1>Hello World</h1>')
-})
+// app.get('/',(req,res) => {
+//     res.send('<h1>Hello World</h1>')
+// })
 
 app.get('/api/notes',(req,res) => {
     res.json(notes)
